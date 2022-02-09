139a36b0a63bdf4f532ef7708bbf2591ded0a29e
dak-384 <dakkofficial@gmail.com>
2022-02-09 17:22:37 +0300

    Initial Commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<form name='contact' method='POST' data-netlify='true'>
+	<p>
+		<label for="">Your Name: 
+			<input type="text" name="name">
+		</label>
+	</p>
+	<p>
+		<label for="email">Your Email:
+			<input type="email" name="email">
+		</label>
+	</p>
+	<p>
+		<label for="message">
+			<textarea name="message"></textarea>
+		</label>
+	</p>
+	<p>
+		<button type="submit">Send</button>
+	</p>
+</form>
+
